87ab6fdb4105f10b8e98bb5495a128c64e2a7202
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2020-08-09 15:26:59 -0400

    Removed recopy utility for hacs since no longer being maintained via submodule.

--- homeassistant/utils/recopy_hacs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-rm -R "$SCRIPT_DIR/../custom_components/hacs"
-cp -R "$SCRIPT_DIR/../submods/hacs__integration/custom_components/hacs" "$SCRIPT_DIR/../custom_components"
