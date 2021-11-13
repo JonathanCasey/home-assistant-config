882526026f8aca068478ddf0438601768ebfbfdd
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2021-11-13 16:56:47 -0500

    Added zigbee network map.

---
+++ homeassistant/www/community/zigbee2mqtt_networkmap/settings.js
@@ -0,0 +1,2 @@
+var webhook_trigger_update_id = '88b64449046af637a757b6b9f287420e2cc460c4c933a16909d8863d77ee6a80';
+var webhook_check_update_id = 'cb0de694fdae4748c18b8e6e76924d76ee6c57b0a03471ca7c554b39ede31e73';
