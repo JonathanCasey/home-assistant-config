7d16464a3d029f58a6f9dd1e60d05059b31b4313
Jonathan Casey <Jonathan.J.Casey@gmail.com>
2022-01-15 16:47:27 -0500

    Zigbee updates.  Maybe don't need to commit?

--- homeassistant/www/community/zigbee2mqtt_networkmap/settings.js
+++ homeassistant/www/community/zigbee2mqtt_networkmap/settings.js
@@ -1,2 +1,2 @@
-var webhook_trigger_update_id = '88b64449046af637a757b6b9f287420e2cc460c4c933a16909d8863d77ee6a80';
-var webhook_check_update_id = 'cb0de694fdae4748c18b8e6e76924d76ee6c57b0a03471ca7c554b39ede31e73';
+var webhook_trigger_update_id = '6c58fa8b9b66e65fe6577bc6e08eac527d188e9bcf2600b49eeb759ff31b39c5';
+var webhook_check_update_id = 'cb65093b2467c5ca759ad593056fb8642ab30d69fc6be6fc05d2b77189081977';
